64197d8f2451bcaf886f1c8218925a8514e130c8
BorisVilhard <150736897+BorisVilhard@users.noreply.github.com>
2024-12-01 14:11:56 +0100

    update: backend

--- controllers/authController.js
+++ controllers/authController.js
@@ -59,3 +59,44 @@ export const handleLogin = async (req, res) => {
 		res.status(500).json({ message: 'Internal server error' });
 	}
 };
+
+export const handleRefreshToken = async (req, res) => {
+	const cookies = req.cookies;
+	if (!cookies?.jwt) return res.sendStatus(401);
+
+	const refreshToken = cookies.jwt;
+
+	try {
+		const foundUser = await User.findOne({ refreshToken }).exec();
+		if (!foundUser) return res.sendStatus(403);
+
+		jwt.verify(
+			refreshToken,
+			process.env.REFRESH_TOKEN_SECRET,
+			(err, decoded) => {
+				if (err || foundUser.username !== decoded.username)
+					return res.sendStatus(403);
+
+				const roles = Object.values(foundUser.roles || {});
+
+				const newAccessToken = jwt.sign(
+					{
+						UserInfo: {
+							id: foundUser._id,
+							username: foundUser.username,
+							email: foundUser.email,
+							roles: roles,
+						},
+					},
+					process.env.ACCESS_TOKEN_SECRET,
+					{ expiresIn: '1d' }
+				);
+
+				res.json({ accessToken: newAccessToken });
+			}
+		);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ message: 'Internal server error' });
+	}
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,14 @@
 import express from 'express';
-import { handleLogin } from '../controllers/authController.js';
+import {
+	handleLogin,
+	handleRefreshToken,
+} from '../controllers/authController.js';
 import { handleGoogleAuth } from '../controllers/googleAuthController.js';
 
 const router = express.Router();
 
 router.post('/', handleLogin);
+router.get('/refresh-token', handleRefreshToken);
 router.post('/google', handleGoogleAuth);
 
 export default router;
